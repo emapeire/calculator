f76e2d346cd5edbe454176f064d6da3bd2827332
emapeire <emapeire@outlook.com>
2022-05-23 22:29:20 -0300

    fixed

--- src/components/boton.jsx
+++ src/components/boton.jsx
@@ -6,7 +6,7 @@ function Boton(props) {
   const esOperador = valor => {
     return isNaN(valor) && (valor !== "=") && (valor !== '.');
   };
-
+  
   return (
     <div
       className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}
